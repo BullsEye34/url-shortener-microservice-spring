33aeb908b1f0c27828002f6fbe7db7bfc601b935
Vamshi Prasad <vamvijkum@gmail.com>
2021-08-12 19:19:38 +0530

    Write Schema for Class

--- src/main/java/ml/vamshiprasad/urlshortener/Link/Schema/Link.java
+++ src/main/java/ml/vamshiprasad/urlshortener/Link/Schema/Link.java
@@ -1,4 +1,31 @@
 package ml.vamshiprasad.urlshortener.Link.Schema;
 
 public class Link {
+    private Long id;
+    private String url;
+
+    public Link(String url) {
+        this.url = url;
+    }
+
+    public Link(Long id, String url) {
+        this.id = id;
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
