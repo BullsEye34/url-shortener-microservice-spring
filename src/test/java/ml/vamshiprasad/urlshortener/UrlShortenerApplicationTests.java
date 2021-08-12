7c7467a78326dd8d2febc9b36fc5ca9cbfad8d5d
Vamshi Prasad <vamvijkum@gmail.com>
2021-08-12 20:25:49 +0530

    All other Files

---
+++ src/test/java/ml/vamshiprasad/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.vamshiprasad.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
